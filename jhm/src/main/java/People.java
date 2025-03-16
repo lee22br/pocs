82ec46f8422f04bc02ee33407c2281fc3e59a5a9
leandro.iglezias <leeiglezias@gmail.com>
2025-03-16 15:12:45 -0300

    JHM init project

---
+++ jhm/src/main/java/People.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class People implements Comparable<People>{
+    private String name;
+    private int age;
+
+    public People(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static List<People> getSample(){
+        List<People> result = new ArrayList<>();
+        People p = new People("Ana", 22);
+        result.add(p);
+        p = new People("Bruna", 21);
+        result.add(p);
+        p = new People("Cintia", 25);
+        result.add(p);
+        p = new People("Daniele", 29);
+        result.add(p);
+        p = new People("Fernanda", 24);
+        result.add(p);
+        p = new People("Ana Clara", 18);
+        result.add(p);
+        p = new People("Ana Maria", 19);
+        result.add(p);
+        return result;
+    }
+
+    @Override
+    public int compareTo(People o) {
+        return this.getName().compareTo(o.getName());
+    }
+}
---
+++ jhm/src/main/java/StreamConcept.java
@@ -0,0 +1,71 @@
+import org.openjdk.jmh.annotations.Benchmark;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+
+public class StreamConcept {
+
+    @Benchmark
+    public static void main (String [] args){
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+        List<People> girls = People.getSample();
+
+        List<Integer> twoEvenSquares =   numbers.stream()
+                .filter(n -> {
+                    System.out.println("filtering " + n);
+                    return n % 2 == 0;
+                })
+                .map(n -> {
+                    System.out.println("mapping " + n);
+                    return n * n;
+                })
+                .limit(2)
+                .toList();
+
+
+        List<People> treeOrderAlfa = girls.stream().limit(3).sorted(Comparator.reverseOrder()).toList();
+        List<People> filterP = girls.stream().filter(p -> p.getName().startsWith("Ana")).toList();
+        List<People> filterAge = girls.stream().filter(p -> p.getAge()>22).sorted(Comparator.comparingInt(People::getAge)).toList();
+        System.out.print("twoEvenSquares: ");
+        twoEvenSquares.forEach(n -> System.out.print(n+ ", "));
+        System.out.print("\n treeReverseOrderAlfa: ");
+        long start = System.currentTimeMillis();
+        treeOrderAlfa.forEach(n -> System.out.print(n.getName()+ ", "));
+        long elapsed = System.currentTimeMillis() - start;
+        System.out.print("\n normal Elapsed: "+elapsed);
+        System.out.print("\n FilterP: ");
+        filterP.forEach(n -> System.out.print(n.getName()+", "));
+        System.out.print("\n filterAge: ");
+        filterAge.forEach(n -> System.out.print(n.getName()+"("+n.getAge()+"), "));
+        //Map
+        List<String> words = Arrays.asList("Oracle", "Java", "Magazine");
+        List<Integer> wordLengths =   words.stream().map(String::length).toList();
+        System.out.print("\n wordLengths: ");
+        wordLengths.forEach(n -> System.out.print(n+","));
+        //Map 2
+        List<Integer> ageList = girls.stream().map(People::getAge).sorted().toList();
+        System.out.print("\n ageList: ");
+        ageList.forEach(n -> System.out.print(n+","));
+
+        // Infinit Stream
+        Stream<Integer> numbers10 = Stream.iterate(0, n -> n + 10);
+        //multi 10 (first 9)
+        List<Integer> multi10 = numbers10.filter(n -> n!=0).limit(9).toList();
+        System.out.print("\n First 9 numbers multi 10: ");
+        multi10.forEach(n -> System.out.print(n+","));
+
+        //Parallel Stream
+        start = System.currentTimeMillis();
+        treeOrderAlfa = girls.parallelStream().limit(3).sorted(Comparator.reverseOrder()).toList();
+        elapsed = System.currentTimeMillis() - start;
+        System.out.print("\n treeReverseOrderAlfa: ");
+        treeOrderAlfa.forEach(n -> System.out.print(n.getName()+ ", "));
+        System.out.print("\n parallel Elapsed: "+elapsed);
+    }
+
+
+
+}
