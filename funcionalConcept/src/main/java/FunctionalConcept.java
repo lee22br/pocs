cd0193e6a76b96ae98d4d2b1c09bb30287d5ba63
lee22br <leeiglezias@gmail.com>
2025-04-03 10:06:37 -0300

    Functional Concepts

--- funcionalConcept/src/main/java/FunctionalConcept.java
+++ funcionalConcept/src/main/java/FunctionalConcept.java
@@ -1,2 +1,17 @@
+import java.util.function.Function;
+
+
 public class FunctionalConcept {
+    private static <T, R> void print(Function<T, R> function, T t ) {
+        System.out.println(function.apply(t));
+    }
+    public static void main(String[] args) {
+        Function <Integer, Integer> cube = x -> x * x * x;
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println("Cube ******");
+        for (int j : array) {
+            print(cube, j);
+        }
+    }
+
 }
